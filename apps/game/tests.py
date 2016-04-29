5f2ad1424bb1f4848f4d1922aa8a87f805574c92
Oleg Shein <oshein@gmail.com>
2016-04-29 15:59:44 +0300

    tests.py add tests

--- apps/game/tests.py
+++ apps/game/tests.py
@@ -1,3 +1,37 @@
+# import unittest
+from django.test import Client
 from django.test import TestCase
 
-# Create your tests here.
+from pprint import pprint as pp
+
+class SimpleTest(TestCase):
+    def test_newgame(self):
+        client = Client()
+        response = client.get('/newGame/')
+        # print response
+        self.assertEqual(response.status_code, 302)
+
+        response = client.get('/')
+        print "GET /","=" * 20
+        # print response
+        print "context=",response.context
+        print "=" * 20
+
+        response = client.post('/startPost/', {'name': 'fred', 'passwd': 'secret'})
+        print "POST /startPost/","=" * 20
+        # print response
+        print "context=",response.context
+        print "=" * 20
+        self.assertEqual(response.status_code, 200)
+
+    # def test_coldroom(self):
+    #     client = Client()
+    #     response = client.get('/coldRoom/')
+    #     # print response
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "<p>You awaken in a prison cell.</p>")
+
+    # def test_index(self):
+    #     client = Client()
+    #     response = client.get('/customer/index/')
+    #     self.assertEqual(response.status_code, 200)
